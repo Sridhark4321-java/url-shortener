72ab213c8f900e262d0d48e9116800a96a52ca75
Sridhark4321-java <sridhark4321@gmail.com>
2024-07-26 15:41:45 -0500

    changes to cache logic

--- url-shortener-api/src/main/java/com/url/shortener/controller/UrlController.java
+++ url-shortener-api/src/main/java/com/url/shortener/controller/UrlController.java
@@ -1,7 +1,8 @@
 package com.url.shortener.controller;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,7 +22,7 @@ public class UrlController {
 
 	@PostMapping("/encode")
 	public ResponseEntity<Object> encode(@RequestBody String url) {
-		Map<String, String> _mapEncode = new HashMap<String, String>();
+		Map<String, String> _mapEncode = new LinkedHashMap<String, String>();
 		_mapEncode.put("Input-URL", url);
 		_mapEncode.put("Shortened-URL", urlService.encodeUrl(url));		
 		return new ResponseEntity<>(_mapEncode,HttpStatus.OK);
@@ -30,7 +31,7 @@ public class UrlController {
 	@PostMapping("/decode")
 	public ResponseEntity<Object> decode(@RequestBody String shortUrl) {
 		if(urlService.decodeUrl(shortUrl) == null) throw new UrlNotFoundException();
-		Map<String, String> _mapDecode = new HashMap<String, String>();
+		Map<String, String> _mapDecode = new LinkedHashMap<String, String>();
 		_mapDecode.put("Input Short-URL", shortUrl);
 		_mapDecode.put("Complete-URL", urlService.decodeUrl(shortUrl));
 		return new ResponseEntity<>(_mapDecode,HttpStatus.OK);
--- url-shortener-api/src/main/java/com/url/shortener/service/UrlServiceImpl.java
+++ url-shortener-api/src/main/java/com/url/shortener/service/UrlServiceImpl.java
@@ -3,6 +3,8 @@ package com.url.shortener.service;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -14,27 +16,42 @@ public class UrlServiceImpl implements UrlService {
 	Map<String, String> _mapShortUrlsCache = new HashMap<String,String>();
 
 	@Override
-	public String encodeUrl(String url) {
-		String encodedUrl = null;
-		StringBuilder sb = new StringBuilder("http://short.est/");
-		// Throw exception when URL not submitted 
+	public String encodeUrl(String url) {	
+		String returnUrl = null;
+		// Throw exception when URL not submitted - for Junit exception case
 		if(url == null)
 			throw new RuntimeException("Invalid URL");	
-		
-		// Generate HashCode
-		String id = Hashing.murmur3_32_fixed().hashString(url, StandardCharsets.UTF_8).toString();
-		
-		encodedUrl = sb.append(id).toString();
-		// Storing short url as key and original url as value.
-		_mapShortUrlsCache.put(encodedUrl.toString(),url);
-		return encodedUrl;
+		// check in cache if found return value from cache otherwise generate one
+		returnUrl = _mapShortUrlsCache.get(url);
+		if( returnUrl != null){
+			//found in cache
+			return returnUrl;
+		} else {		
+			//creating new in cache
+			StringBuilder sb = new StringBuilder("http://short.est/");
+			
+			// Generate HashCode
+			String id = Hashing.murmur3_32_fixed().hashString(url, StandardCharsets.UTF_8).toString();
+			
+			returnUrl = sb.append(id).toString();
+			// Storing  original url as key and short url as value.
+			_mapShortUrlsCache.put(url,returnUrl.toString());
+
+		}
+		return returnUrl;
 	}
 
 	@Override
 	public String decodeUrl(String shortUrl) {
+		String decodedUrl = null;
+
 		//Read the original/decoded url from cache and return when match found
-		String origUrl = _mapShortUrlsCache.get(shortUrl);
-		return origUrl;
+		Optional<Entry<String, String>> origUrl = _mapShortUrlsCache.entrySet().stream().filter(x->x.getValue().equals(shortUrl)).findFirst();
+		
+		if (origUrl.isPresent())				
+			decodedUrl = origUrl.get().getKey();
+		
+		return decodedUrl;
 	}
 
 
